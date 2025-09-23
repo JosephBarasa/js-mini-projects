9d088034f4290c1b96fcb83b0c27fbc92ea9ff1d
Joseph Barasa <joephbarasa622@gmail.com>
2025-09-23 15:21:02 +0300

    functions

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//ARRAYS
+
+const names = ["Joseph", "Jesse", "Jean", "Japheth"];
+const names2 = ["Mercy", "Mary", "Sam", "Derick"];
+
+let sort = names.sort();
+console.log(sort);
--- text-based-calculator.js
+++ text-based-calculator.js
@@ -1,42 +1,51 @@
 //TEXT-BASED CALCULATOR
 
 const prompt = require("prompt-sync")();
-let leftOperand = prompt("Enter first number : ");
-const operator = prompt("Enter operator : ");
-let rightOperand = prompt("Enter second number : ");
 
-leftOperand = parseInt(leftOperand);
-rightOperand = parseInt(rightOperand);
-
-while(isNaN(leftOperand) || isNaN(rightOperand) || leftOperand === '' || rightOperand === '') {
-    console.log('Please enter a valid number.');
-    let leftOperand = prompt("Enter first number : ");
-    const operator = prompt("Enter operator : ");
-    let rightOperand = prompt("Enter second number : ");
+function getNumber(numberString) {
+    while (true) {
+    number = parseFloat(prompt(`Enter Number ${numberString} : `));
+    if (isNaN(number)) {
+        console.log(`${number} is not a valid number.`);
+    } else {
+        return number;
+    }
 }
+}
+ 
 
+const leftOperand = getNumber("One");
+
+const operator = (prompt("Enter Operator : ")); 
+
+const rightOperand = getNumber("Two");
+
+let result;
+let valid = true;
 switch (operator) {
     case '+':
         result = leftOperand + rightOperand;
-        console.log(`The result is : ${result} `);
         break;
     case '-':
         result = leftOperand - rightOperand;
-        console.log(`The result is : ${result}`);
         break;
     case '*':
         result = leftOperand * rightOperand;
-        console.log(`The result is : ${result}`);
         break;
     case '%':
         result = leftOperand % rightOperand;
-        console.log(`The result is : ${result}`);
         break;
     case '/':
-        result = leftOperand / rightOperand;
-        console.log(`The result is : ${result}`);
+        if(leftOperand === 0 || rightOperand === 0) {
+            valid = false;
+            console.log('Zero Division Error !');
+        } else {
+            result = leftOperand / rightOperand;
+        }
         break;
     default:
+        valid = false;
         console.log('Please enter a valid operator.');
 }
 
+if (valid) console.log(`${leftOperand} ${operator} ${rightOperand} = ${result}`);

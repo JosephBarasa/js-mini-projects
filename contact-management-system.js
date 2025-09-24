9340575b53efed8e58161d81fd3dee20f2eaf9f1
Joseph Barasa <joephbarasa622@gmail.com>
2025-09-24 11:42:27 +0300

    delete contact

--- contact-management-system.js
+++ contact-management-system.js
@@ -3,9 +3,9 @@
 const prompt = require('prompt-sync')();
 
 function getInfo() {
-    console.log('--------------------------');
-    console.log('CONTACT MANAGEMENT SYSTEM');
-    console.log('--------------------------');
+    console.log('------------------------------------------');
+    console.log('********CONTACT MANAGEMENT SYSTEM********');
+    console.log('------------------------------------------');
     console.log('1. Add Contact');
     console.log('2. Delete Contact');
     console.log('3. View Contacts');
@@ -16,31 +16,55 @@ function getInfo() {
 const contacts = [];
 
 function addContact() {
+    console.log('-----------');
+    console.log('Add Contact');
+    console.log('-----------');
     let name = prompt("Enter Name : ");
     let phone = prompt("Enter Phone Number : ");
     const contactInfo = {name: name, phone: phone};
     contacts.push(contactInfo);
-    console.log('Added')
+    console.log('***ADDED***')
 }
 
-function deleteContact() {}
+function deleteContact() {
+    console.log('-----------');
+    console.log('Delete Contact');
+    console.log('-----------');
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        console.log(`${i + 1}. ${contact.name}`);
+    }
+    const number = prompt("SELECT THE NUMBER OF THE CONTACT TO BE DELETED : ");
+    if (isNaN(number) || number > contacts.length) {
+        console.log(`PLEASE ENTER A VALID NUMBER : (1 - ${contacts.length})`);
+        return;
+    }
+    contacts.splice(number - 1);
+    console.log("CONTACT DELETED");
+}
 
 function viewContact(contacts) {
+    console.log('-----------');
+    console.log('Contacts');
+    console.log('-----------');
     for(let contact of contacts) {
-        console.log("############");
+        console.log("###");
         console.log("Name : " , contact.name);
         console.log("Phone : " , contact.phone);
     }
 }
 
-function searchContact() {}
+function searchContact() {
+    console.log('-----------');
+    console.log('Search Contact');
+    console.log('-----------');
+}
 
 getInfo();
 
 let continueToApp = true;
 while(continueToApp) {
-    console.log("############");
-    const number = prompt("Select a number to perform operation : ");
+    const number = prompt("SELECT A NUMBER TO PERFORM OPERATION : ");
 
     switch(number) {
         case '1':
